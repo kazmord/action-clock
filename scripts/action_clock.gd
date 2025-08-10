c28371c705ac8cda5283ffd1a027cb84884b0a2d
kazmord <kazmord@gmail.com>
2025-08-10 23:55:20 +0100

    action slot wip

--- scripts/action_clock.gd
+++ scripts/action_clock.gd
@@ -10,7 +10,7 @@ var slot_positions = []
 @onready var clock_hand: TextureRect = $ClockHand
 @onready var circle_ray: RayCast2D = $CircleRay
 
-var test: PackedScene = preload("res://scenes/test_sprite.tscn")
+var action_slot: PackedScene = preload("res://scenes/action_clock/action_slot.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -30,11 +30,13 @@ func _move_clock(delta: float) -> void:
 # TODO: Optimise
 # instead of checking all slots iteratively, identify the "next slot".
 # when clock position exceeds slot position, make the slot after it the "next slot"
-# TODO 2: Actually do something more than printing
+# TODO 2: Check slots instead of raw floats
+# TODO 3: Actually do something more than printing
 func _check_slot_reached(slots: Array):
 	for slot in slots:
-		if previous_clock_rotation_degrees < slot and slot < clock_rotation_degrees:
+		if previous_clock_rotation_degrees < slot.clock_rotation and slot.clock_rotation < clock_rotation_degrees:
 			print(slot)
+			slot.try_do_action()
 
 func _place_slots(slot_number: int) -> void:
 	if slot_number < 1:
@@ -45,9 +47,10 @@ func _place_slots(slot_number: int) -> void:
 	for slot in slot_number:
 		var slot_position = slot_interval * slot + slot_interval / 2
 		circle_ray.rotation_degrees = slot_position
-		var t_sprite = test.instantiate() #TODO: use actual slots
-		t_sprite.position = circle_ray.position + circle_ray.target_position.rotated(circle_ray.rotation)
-		add_child(t_sprite)
-		slot_array.append(slot_position)
-		move_child(clock_hand, -1)
+		var s = action_slot.instantiate()
+		s.position = circle_ray.position + circle_ray.target_position.rotated(circle_ray.rotation) - s.size / 2
+		s.clock_rotation = slot_position
+		add_child(s)
+		slot_array.append(s)
 	slot_positions = slot_array
+	move_child(clock_hand, -1)
--- scripts/action_slot.gd
+++ scripts/action_slot.gd
@@ -1,6 +1,7 @@
 class_name ActionSlot extends PanelContainer
 
 @export var action: Action = ActionNull.new()
+@export var clock_rotation: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,3 +11,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func try_do_action() -> void:
+	action.do_action()
